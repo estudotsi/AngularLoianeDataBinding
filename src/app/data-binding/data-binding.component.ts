0f43614e01e93a37c370c542446e6b8b50c7f761
unknown <estudotsi@gmail.com>
2022-06-21 21:47:26 -0300

    até property binding e interpolação

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url: string = "teste.com";
+  cursoAngular: boolean = true;
+  urlImagem = 'https://picsum.photos/200';
+
+  getValor(){
+    return 1;
+  }
+  getCurtirCurso(){
+    return true;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
